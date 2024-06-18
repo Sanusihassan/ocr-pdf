59b8b0266056b8fbc4c391dfee16aa152a3c43af
Sanusi Hassan <sanusihassan122@gmail.com>
2024-06-19 01:09:08 +0200

    fixed language issue

--- components/EditArea/SubmitBtn.tsx
+++ components/EditArea/SubmitBtn.tsx
@@ -19,6 +19,9 @@ export function SubmitBtn({
   const isSubmitted = useSelector(
     (state: { tool: ToolState }) => state.tool.isSubmitted
   );
+  const selectedLanguages = useSelector(
+    (state: { tool: ToolState }) => state.tool.selectedLanguages
+  );
   return (
     <button
       className={`submit-btn btn btn-lg text-white position-relative overflow-hidden ${k} grid-footer`}
@@ -29,7 +32,7 @@ export function SubmitBtn({
           submitBtn?.current?.click();
         }
       }}
-      disabled={errorMessage.length > 0}
+      disabled={errorMessage.length > 0 || !selectedLanguages.length}
     >
       <bdi>
         {
